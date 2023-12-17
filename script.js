a83a823b55e7b3c80d4eb38ebd4d8473176cf466
ItIsMLiu <mliu.design@outlook.com>
2023-12-17 18:56:44 +0000

    Created function for display weather info.

--- script.js
+++ script.js
@@ -9,79 +9,68 @@ $(document).ready(function(){
 
     let searchHistory = [];
 
-    // Search Button when clicked ...
-    searchBtn.on('click', function(event){
-        event.preventDefault();
+    // Function to get today's weather
+    function todayWeather(data) {
+        let todayList = $("<ul>");
+        let today = dayjs();
+        let currentCity = $("<li>").text(data.city.name + " - " + data.city.country + " (" + today.format("DD/MM/YYYY") + ")");
+        currentCity.addClass('current-city');
 
-        // Clear previous search result
-        $("#today").empty();
-        $("#forecast").empty();
-        
+        let iconCode = data.list[0].weather[0].icon;
+        let iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+        let iconImg = $("<img>").attr('src', iconURL);
+        let currentWeather = $("<li>").text("Current Weather: " + data.list[0].weather[0].description);
+
+        let currentTempC = $("<li>").text("Temp: " + (data.list[0].main.temp - 273.15).toFixed(0) + "°C");
+        let currentWindSpeed = $("<li>").text("Wind: " + data.list[0].wind.speed + " KPH");
+        let currentHumidity = $("<li>").text("Humidity: " + data.list[0].main.humidity + "%");
+        $("#today").append(todayList);
+        todayList.append(currentCity, currentWeather, currentTempC, currentWindSpeed, currentHumidity);
+        currentWeather.append(iconImg);
+    }
+    // Function to get next 5 days' weather
+    function forecastWeather(data) {
+        let fiveDay = $("<h5>").text("5-Day Forecast:");
+        $("#forecast").append(fiveDay);
+
+        for (let i = 7; i < 41; i += 8) {
+            
+            let forecastList = $("<ul>");
+            forecastList.addClass('col');
+
+            let forecastDate = $("<li>").text(data.list[i].dt_txt);
+            
+            let forecastIconCode = data.list[i].weather[0].icon;
+            let forecastIconURL = "http://openweathermap.org/img/wn/" + forecastIconCode + "@2x.png";
+            let forecastIconImg = $("<img>").attr('src', forecastIconURL);
+            var forecastIconLi = $("<li>");
+            forecastIconLi.append(forecastIconImg);
+            
+            let forecastTempC = $("<li>").text("Temp: " + (data.list[i].main.temp - 273.15).toFixed(0) + "°C");
+            let forecastWindSpeed = $("<li>").text("Wind: " + data.list[i].wind.speed + " KPH");
+            let forecastHumidity = $("<li>").text("Humidity: " + data.list[i].main.humidity + "%");
+            $("#forecast").append(forecastList);
+            forecastList.append(forecastDate, forecastIconLi, forecastTempC, forecastWindSpeed, forecastHumidity);
+        }
+    }
+    // Function to render buttons
+    function renderBtns() {
+        let historyDiv = $("#history");
+        historyDiv.empty();
+        for (let i = 0; i < searchHistory.length; i++) {
+            let historyBtn = $("<button>");
+            historyBtn.addClass("btn btn-secondary");
+            historyBtn.attr("data-name", searchHistory[i]);
+            historyBtn.text(searchHistory[i]);
+            historyDiv.prepend(historyBtn); 
+        }
+    }
+    // Function to fetch and display weather info
+    function displayWeatherInfo() {
         // Building the URL we need to query the database
-        // Get the value of #search-input
-        cityName = $('#search-input').val().trim();
-        searchHistory.push(cityName);
         queryURL = apiURL + "q=" + cityName + "&appid=" + apiKey;
         console.log(queryURL)
 
-        // Function to get today's weather
-        function todayWeather(data){
-            let todayList = $("<ul>");
-            let today = dayjs();
-            let currentCity = $("<li>").text(data.city.name + " - " + data.city.country + " (" + today.format("DD/MM/YYYY") + ")");
-            currentCity.addClass('current-city');
-
-            let iconCode = data.list[0].weather[0].icon;
-            let iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-            let iconImg = $("<img>").attr('src', iconURL);
-            let currentWeather = $("<li>").text("Current Weather: " + data.list[0].weather[0].description);
-
-            let currentTempC = $("<li>").text("Temp: " + (data.list[0].main.temp - 273.15).toFixed(0) + "°C");
-            let currentWindSpeed = $("<li>").text("Wind: " + data.list[0].wind.speed + " KPH");
-            let currentHumidity = $("<li>").text("Humidity: " + data.list[0].main.humidity + "%");
-            $("#today").append(todayList);
-            todayList.append(currentCity, currentWeather, currentTempC, currentWindSpeed, currentHumidity);
-            currentWeather.append(iconImg);
-        }
-        // Function to get next 5 days' weather
-        function forecastWeather(data){
-            let fiveDay = $("<h5>").text("5-Day Forecast:");
-            $("#forecast").append(fiveDay);
-
-            for (let i = 7; i < 41; i += 8) {
-                
-                let forecastList = $("<ul>");
-                forecastList.addClass('col');
-
-                let forecastDate = $("<li>").text(data.list[i].dt_txt);
-                
-                let forecastIconCode = data.list[i].weather[0].icon;
-                let forecastIconURL = "http://openweathermap.org/img/wn/" + forecastIconCode + "@2x.png";
-                let forecastIconImg = $("<img>").attr('src', forecastIconURL);
-                var forecastIconLi = $("<li>");
-                forecastIconLi.append(forecastIconImg);
-                
-                let forecastTempC = $("<li>").text("Temp: " + (data.list[i].main.temp - 273.15).toFixed(0) + "°C");
-                let forecastWindSpeed = $("<li>").text("Wind: " + data.list[i].wind.speed + " KPH");
-                let forecastHumidity = $("<li>").text("Humidity: " + data.list[i].main.humidity + "%");
-                $("#forecast").append(forecastList);
-                forecastList.append(forecastDate, forecastIconLi, forecastTempC, forecastWindSpeed, forecastHumidity);
-            }
-        }
-        // Function to render buttons
-        function renderBtns() {
-            let historyDiv = $("#history");
-            historyDiv.empty();
-            for (let i = 0; i < searchHistory.length; i++) {
-                let historyBtn = $("<button>");
-                historyBtn.addClass("btn btn-secondary");
-                historyBtn.attr("data-name", searchHistory[i]);
-                historyBtn.text(searchHistory[i]);
-                historyDiv.prepend(historyBtn); 
-            }
-        }
-
-        // Fetch third party data
         fetch(queryURL)
         .then(function(response) {
             return response.json();
@@ -92,6 +81,20 @@ $(document).ready(function(){
             forecastWeather(data);
             renderBtns();
         });
+    }
+    // Search Button when clicked ...
+    searchBtn.on('click', function(event){
+        event.preventDefault();
+
+        // Clear previous search result
+        $("#today").empty();
+        $("#forecast").empty();
+        
+        // Get the value of #search-input
+        cityName = $('#search-input').val().trim();
+        searchHistory.push(cityName);
+
+        displayWeatherInfo()
     }); 
 }); 
 
